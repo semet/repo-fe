de84e5545c180d1f103fa7b6f7c27b04a055eac3
Hamdani <hamdani@synergizetech.dev>
2024-11-17 08:17:21 +0800

    add debugger to xior

--- app/libs/http-instance.ts
+++ app/libs/http-instance.ts
@@ -1,37 +1,43 @@
 import { isServer } from '@tanstack/react-query'
-import xior, { merge } from 'xior'
+import xior, { merge, XiorInstance } from 'xior'
+
+let instance: XiorInstance
 
 const HttpInstance = (token?: string) => {
-  const baseURL = isServer ? process.env.API_URL : window.ENV.API_URL
-  const apiKey = isServer ? process.env.API_KEY : window.ENV.API_KEY
-  const instance = xior.create({
-    baseURL
-  })
-  instance.interceptors.request.use((config) => {
-    if (apiKey) {
-      config.headers['x-data-reference'] = apiKey
-    }
+  if (!instance) {
+    const baseURL = isServer ? process.env.API_URL : window.ENV.API_URL
+    const apiKey = isServer ? process.env.API_KEY : window.ENV.API_KEY
 
-    return merge(config, {
-      headers: {
-        ...(apiKey && { 'x-data-reference': apiKey }),
-        'x-device': config.headers['x-device'] ?? 'desktop',
-        ...(token && { Authorization: `Bearer ${token}` })
-      }
+    instance = xior.create({
+      baseURL
     })
-  })
 
-  instance.interceptors.response.use(
-    (response) => {
-      return response
-    },
-    (error) => {
-      if (typeof window !== 'undefined' && error?.response?.status === 401) {
-        window.location.href = '/'
+    // Request interceptor
+    instance.interceptors.request.use((config) => {
+      // eslint-disable-next-line no-console
+      console.log(`🚀🚀🚀Request >> ${config.url}`)
+      return merge(config, {
+        headers: {
+          ...(apiKey && { 'x-data-reference': apiKey }),
+          'x-device': config.headers['x-device'] ?? 'desktop',
+          ...(token && { Authorization: `Bearer ${token}` })
+        }
+      })
+    })
+
+    // Response interceptor
+    instance.interceptors.response.use(
+      (response) => {
+        return response
+      },
+      (error) => {
+        if (typeof window !== 'undefined' && error?.response?.status === 401) {
+          window.location.href = '/'
+        }
+        return Promise.reject(error)
       }
-      return Promise.reject(error)
-    }
-  )
+    )
+  }
 
   return instance
 }
